fd71e3e4440efee7d90d480926690489878bcbb1
Samuel Gómez Pérez <samgope100@gmail.com>
2022-12-13 14:23:28 +0100

    cambios hechos en clase

--- index.js
+++ index.js
@@ -3,15 +3,13 @@ if(localStorage.recordatorios != null)
 else
     var recordatorios = [];
 
+var recordatorios_pendientes = 0;
 
 $(document).ready(function(){
 
     var cant_recordatorios = recordatorios.length;
-    var recordatorios_pendientes = 0;
 
     //Enter
-    // var input = document.getElementById("input");
-    // if(input.text != null){
         $(input).keypress(function(e){
             var keycode = (e.keyCode ? e.keyCode : e.which);
             if (keycode == "13") {
@@ -22,9 +20,7 @@ $(document).ready(function(){
                 $("#cantidad_pendientes").text(`${recordatorios_pendientes} pendientes de ${cant_recordatorios}`);
             }
         });
-    // }
         
-
     //Si hay localStorage lo muestra
     if(localStorage.recordatorios != null){
         mostrarLocalStorage();
@@ -32,10 +28,21 @@ $(document).ready(function(){
 
     //Marcar recordatorio como hecho
     $("#lista").on("click", "#hecho", function(){
-        $(this).css("border-color", "black");
-        $(this).siblings().css("text-decoration", "line-through");
-        recordatorios_pendientes --;
-        $("#cantidad_pendientes").text(`${recordatorios_pendientes} pendientes de ${cant_recordatorios}`)
+        var recordatorio = recordatorios[$(this).parent().index()]; 
+        if(recordatorio.completado == false){
+            recordatorio.completado = true;
+            recordatorios_pendientes --;
+            $(this).css("border-color", "black");
+            $(this).siblings().css("text-decoration", "line-through");
+        }
+        else{
+            recordatorio.completado = false;
+            recordatorios_pendientes ++;
+            $(this).css("border-color", "white");
+            $(this).siblings().css("text-decoration", "none");
+        }
+        localStorage.recordatorios = JSON.stringify(recordatorios);
+        $("#cantidad_pendientes").text(`${recordatorios.length + recordatorios_pendientes} pendientes de ${cant_recordatorios}`)
     });
 
     //Borrar recordatorio
@@ -57,13 +64,15 @@ $(document).ready(function(){
 
     //Borrar Tareas
     $("#borrarTareas").click(function(){
-        $("li").fadeOut("normal", function(){
-            $("li").remove()
-        });
-        localStorage.clear();
-        cant_recordatorios = 0;
-        recordatorios_pendientes = 0;
-        recordatorios = [];
+        for(var i = 0; i < recordatorios.length; i++){
+            if(recordatorios[i].completado == true){
+                var li = $("li");
+                li[i].remove();
+                i --;
+                recordatorios.splice(i+1, 1);
+            }
+        }
+        localStorage.recordatorios = JSON.stringify(recordatorios);
         $("#cantidad_pendientes").text(`${recordatorios_pendientes} pendientes de ${cant_recordatorios}`);
     });
 
@@ -78,6 +87,7 @@ function AnadirRecordatorio(){
         var futuro_recordatorio = {
             contenido: n_recordatorio,
             prioridad: "normal",
+            completado: false,
             date: Date.now()
         };
         var li = $(`<li id='recordatorio'><button id='hecho'></button><p id='contenido'>` + n_recordatorio + `</p><div id='prioridades'>Prioridad:<button class='prioridad' id='low'>↓Low</button><button class='prioridad' id='normal'>Normal</button><button class='prioridad' id='high'>High↑</button></div><div id='tiempo'><p>⌛Añadido hace ${Math.floor(((Date.now() - futuro_recordatorio.date)/1000)/60)} minutos</p></div><i class='fa-solid fa-square-minus'></i></li>`);
@@ -104,11 +114,33 @@ function cambiarPrioridad(){
 //Función para mostrar el localStorage
 function mostrarLocalStorage(){
     var recordatorios = JSON.parse(localStorage.getItem("recordatorios"));
+    var pendientes = 0;
     for(var i = 0; i < recordatorios.length; i++){
         var ul = $("ul");
         var li = $(`<li id='recordatorio'><button id='hecho'></button><p id='contenido'>` + recordatorios[i].contenido + `</p><div id='prioridades'>Prioridad:<button class='prioridad' id='low'>↓Low</button><button class='prioridad' id='normal'>Normal</button><button class='prioridad' id='high'>High↑</button></div><div id='tiempo'><p>⌛Añadido hace ${Math.floor(((Date.now() - recordatorios[i].date)/1000)/60)} minutos</p></div><i class='fa-solid fa-square-minus'></i></li>`);
+        if(recordatorios[i].completado == false){
+            $(li).find("#hecho").css("border-color", "white");
+            $(li).find("#contenido").css("text-decoration", "none");
+            pendientes ++;
+        }
+        else if(recordatorios[i].completado == true){
+            $(li).find("#hecho").css("border-color", "black");
+            $(li).find("#contenido").css("text-decoration", "line-through");
+        }
         $(ul).append(li);
+        if(recordatorios[i].prioridad == "low"){
+            $(li).find("#low").css("background-color", "green");
+            $(li).find("#low").css("color", "white");
+        }
+        else if(recordatorios[i].prioridad == "normal"){
+            $(li).find("#normal").css("background-color", "green");
+            $(li).find("#normal").css("color", "white");
+        }
+        else if(recordatorios[i].prioridad == "high"){
+            $(li).find("#high").css("background-color", "green");
+            $(li).find("#high").css("color", "white");
+        }
     }
     var cant_pendientes = document.getElementById("cantidad_pendientes");
-    $(cant_pendientes).text(`${recordatorios.length} pendientes de un total de ${recordatorios.length}`);
+    $(cant_pendientes).text(`${pendientes} pendientes de un total de ${recordatorios.length}`);
 }
